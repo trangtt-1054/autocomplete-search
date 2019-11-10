1d9a6321863936ac69fafae62b695cbb8c72a333
Tran Thu Trang <tranthutrang@Trans-MacBook-Pro.local>
2019-11-10 18:56:04 +0700

    All

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {
+  
+  MatIconModule, MatInputModule,
+  MatAutocompleteModule,MatChipsModule,
+  MatFormFieldModule
 
+  
+} from '@angular/material';
+import { HomePageComponent } from './component/homepage/homepage.component';
+import { SearchbarComponent } from './component/searchbar/searchbar.component';
+import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SearchbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule,
+     MatIconModule, MatInputModule,
+    MatAutocompleteModule,
+   MatChipsModule,
+    MatFormFieldModule
+
+    
+    
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
